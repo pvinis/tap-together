93032c7b12e2589ce35b23f67a52dffa05138c3e
Pavlos Vinieratos <pvinis@gmail.com>
2023-12-07 11:40:46 +0200

    aha

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,6 +3,13 @@ import { getAuth } from "firebase/auth"
 import { getFirestore, collection, getDocs } from "firebase/firestore"
 
 const firebaseConfig = {
+	apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+	projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 }
 
 export const app = initializeApp(firebaseConfig)
